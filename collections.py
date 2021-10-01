379a6c0cece266556c4a75866b1bd33eb17f1c90
Leon L. Robinson <leon.robinson@qa.com>
2021-10-01 14:22:48 +0100

    Collections

---
+++ collections.py
@@ -0,0 +1,25 @@
+
+
+
+varOfShopping = ('meat', 'veg', 'cake', 'beer for the weekend')
+emptyList = []
+
+cool_cows = ["Winnie the Moo", "Moolan", "Milkshake", "Mooana"]
+cool_sheep = ["Baaaart", "Baaaarnaby"]
+cool_pigs = ["Chris P. Bacon", "Hamlet", "Hogwarts"]
+
+cool_pigs.insert(1,1975)
+cool_pigs.remove("Hamlet")
+cool_animals = [cool_cows, cool_sheep, cool_pigs]
+
+
+
+listOfSomeThings = ('cat','cat','dog','sheep','cat')
+countListVar = len("This is a string")
+#print(countListVar)
+
+varForADictionary = { 'name':'Leon' , 'size':'nearly big', 'age':'I am still young' }
+
+varForADictionary['FaveCol'] = 'Brown'
+
+print(varForADictionary)
