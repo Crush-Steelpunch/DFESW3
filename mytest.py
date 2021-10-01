c0d3dd7bc5fa0166990fcfd44a98a0ca9a8d7229
Leon L. Robinson <leon.robinson@qa.com>
2021-10-01 11:56:31 +0100

    Introductory stuff

---
+++ mytest.py
@@ -0,0 +1,19 @@
+
+variableForAnInput = int(input("Enter A Value To Be Put In the Variable: "))
+
+variableForAString = "this is a string"
+variableForAnotherString = "this is another string"
+variableForAFloat = 15.2
+variableForAninteger = 121
+variableForABoolean = True
+
+
+print("I can python")
+
+#print(2 + 2)
+#print(variableForAString + variableForAnotherString)
+#print(variableForAFloat + variableForAninteger)
+#print(variableForAString + " "  + str(variableForAFloat))
+#print(f"type some text: {variableForAString} {variableForAFloat}")
+
+print(variableForAString + " " +  variableForAnInput)
