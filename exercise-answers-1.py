3c3f85fd95136d7365ace4b23f4be88ab38abfe1
Leon L. Robinson <leon.robinson@qa.com>
2021-10-01 13:47:22 +0100

    exercise answers1

---
+++ exercise-answers-1.py
@@ -0,0 +1,43 @@
+# Introduction
+
+# What will be the output of this code?
+number1 = input("Enter whole number: ")     # 57
+number2 = input("Enter decimal number: ")   # 12.1 
+
+integer_number = int(number1)
+float_number = float(number2)
+round_number = int(round(float_number))
+
+print(number1)   
+print(number2)
+print(round_number)
+
+name = "Pep Guardogiola"
+age = 3
+bark = True
+tweet = False
+
+print("My pet is called", name, ", He is", age, "years old.")
+print("Statement:", name, "barks.", bark)
+print("Statement:", name, "tweets.", tweet)
+
+
+PRINT "Hello World"
+print(Hello World)
+Print("Hello World")
+priint("Hello World")
+
+# Polite program
+
+firstname = "Biff"
+lastname = input("Please enter your last name")
+print("Hello " + firstname + " " + lastname)
+
+
+
+number1 = int(input("Please enter the first number: "))
+number2 = int(input("Please enter the second number: "))
+answer = number1 + number2
+
+print(number1, "+", number2, "=", answer)
+
