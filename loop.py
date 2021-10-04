1150240dfc920d8f2daa038728f3030c2a062620
Leon L. Robinson <leon.robinson@qa.com>
2021-10-04 13:49:21 +0100

    looping things while loop

--- loop.py
+++ loop.py
@@ -1,21 +1,43 @@
-# While and for 
-
-# for loop
+## While and for 
+#
+## for loop
 books = ['Winnie the Pooh','War and Peace', '1984', 'My family and other animals', 'Boy', 'Art of War', 'Of Mice and Men', 'Peter and Jane']
 
 for bookItem in books:
     if bookItem == '1984':
-        print(bookItem + " is the only book in the list that can be cast to int()")
-    else:
-        print(bookItem)
+        continue
+    print(bookItem)
 
-strVar = '01101010100'
-for thisIsAVar in strVar:
+ strVar = '01101010100'
+ for thisIsAVar in strVar:
     if int(thisIsAVar):
         print('This was False')
     else:
         print('The Truth!')
 
-for numVar in range(100):
+
+ for numVar in range(100):
     print(numVar)
 
+#
+
+
+varForADictionary = { 'name':'Leon' , 'size':'nearly big', 'age':'I am still young' }
+
+for thingsVar in varForADictionary.keys():
+    print("The key is " + thingsVar + ", The value is: "+ varForADictionary[thingsVar])
+
+
+
+# While 
+
+
+
+
+inputNumVar = int(input('Shove a whole number in: '))
+resultVar = 1
+while inputNumVar > 0:
+    resultVar = resultVar * inputNumVar
+    inputNumVar = inputNumVar - 1
+
+print(resultVar)
